4dd05e3c046d9eca065346e91789336230c8bb10
bespoke-itadmin-2023 <jomer.bespoke@gmail.com>
2024-12-07 00:46:54 +0800

    fix

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,7 +1,7 @@
 // app/middleware.js
 import { NextResponse } from 'next/server';
 
-const allowedOrigins = ['https://www.velosai.pro', '*'];
+const allowedOrigins = ['https://www.velosai.pro/', '*'];
 
 export function middleware(request) {
     const origin = request.headers.get('origin');
@@ -13,6 +13,7 @@ export function middleware(request) {
         response.headers.set('Access-Control-Allow-Origin', origin); // Allow the requesting origin
         response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Methods allowed
         response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Headers allowed
+        response.headers.set('Access-Control-Allow-Credentials', 'true'); // Allow credentials
     }
 
     return response;
